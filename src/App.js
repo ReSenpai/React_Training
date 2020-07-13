30f41bb018bb6f983b3184f20d402a1f667ce16f
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-07-13 15:32:11 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li>Css</li>
+        <li>html</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App;
