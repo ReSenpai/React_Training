ebd7abd43c5367b878360040eace2c79a8f481e3
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-07-15 23:38:09 +0300

    Create Setting.jsx

---
+++ src/components/Setting/Setting.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Setting.module.css'
+
+const Setting = (props) => {
+    return (
+        <div>
+            Setting
+        </div>
+    )
+}
+
+export default Setting;
