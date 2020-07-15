c6cd0373f3a87b82933b93be08d1200dcd17ee7f
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-07-15 23:37:16 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Profile from './components/Profile';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Setting from './components/Setting/Setting';
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Route path='/messages' component={Dialogs} />
+          <Route path='/profile' component={Profile} />
+          <Route path='/news' component={News} />
+          <Route path='/music' component={Music} />
+          <Route path='/setting' component={Setting} />
+        </div>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
