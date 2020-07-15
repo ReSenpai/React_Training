45b6ccc4472549dcdfdfe5814a0a358461ddf43d
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-07-15 23:51:39 +0300

    Build

---
+++ build/precache-manifest.d17b81470965f93c5dcb752304361e9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88514bc424d9bece988c07d15db42270",
+    "url": "/index.html"
+  },
+  {
+    "revision": "302c637f7c3969723aeb",
+    "url": "/static/css/main.854bd93e.chunk.css"
+  },
+  {
+    "revision": "164ccafffcd515c11f85",
+    "url": "/static/js/2.bd45c7f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bd45c7f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "302c637f7c3969723aeb",
+    "url": "/static/js/main.be34400b.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
