bc62ae056312dde74cee8fe6e224d6ef7379ba3c
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-07-17 15:51:36 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ const App = () => {
         <Header />
         <Navbar />
         <div className='app-wrapper-content'>
-          <Route path='/profile' component={Profile} />
-          <Route path='/messages' component={Dialogs} />
-          <Route path='/news' component={News} />
-          <Route path='/music' component={Music} />
-          <Route path='/setting' component={Setting} />
+          <Route path='/profile' render={ () => <Profile /> } />
+          <Route path='/messages' render={ () => <Dialogs /> } />
+          <Route path='/news' render={ () => <News /> } />
+          <Route path='/music' render={ () => <Music /> } />
+          <Route path='/setting' render={ () => <Setting /> } />
         </div>
       </div>
     </HashRouter>
