cb2480ed58036f44ac808d5191ade5d7110ea717
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-07-18 00:29:38 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,11 +3,11 @@ import style from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 
-const Profile = () => {
+const Profile = (props) => {
     return (
         <div className={style.wrapper}>
             <ProfileInfo />
-            <MyPosts />
+            <MyPosts posts={props.state.posts} />
         </div>
     );
 }
