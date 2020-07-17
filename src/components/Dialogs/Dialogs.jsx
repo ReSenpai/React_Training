976e3b87e15b29c85ae729475e1eec1966f38fbb
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-07-18 00:29:23 +0300

    Update Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,31 +5,19 @@ import Message from './Message/Message';
 
 const Dialogs = (props) => {
 
-    let dialogsData = [
-        {id: 1, name: 'Dimych'},
-        {id: 2, name: 'Sveta'},
-        {id: 3, name: 'Valera'},
-        {id: 4, name: 'Polya'},
-        {id: 5, name: 'Anna'},
-        {id: 6, name: 'Oliva'},
-        {id: 7, name: 'Korina'},
-    ]
+    let dialogElements = props.state.dialogs
+    .map( (dialog, i) => <DialogItem 
+        key={ i } 
+        name={ dialog.name } 
+        id={ dialog.id } 
+        avatar={ dialog.avatar } 
+    />)
 
-    let messages = [
-        {id: 1, message: 'Hello'},
-        {id: 2, message: "I don't no"},
-        {id: 3, message: 'Abrakadabra'},
-        {id: 4, message: 'Step for my sister'},
-        {id: 5, message: 'Omnon nim'},
-        {id: 6, message: 'Nuaah'},
-        {id: 7, message: 'Poly'},
-    ]
-
-    let dialogElements = dialogsData
-    .map( (dialog, i) => <DialogItem key={i} name={ dialog.name } id={ dialog.id } />)
-
-    let messagesElement = messages
-    .map( (message, i) => <Message key={i} message={message.message} />)
+    let messagesElement = props.state.messages
+    .map( (message, i) => <Message 
+        key={ i } 
+        message={ message.message } 
+    />)
 
     return (
         <div className={style.dialogs}>
