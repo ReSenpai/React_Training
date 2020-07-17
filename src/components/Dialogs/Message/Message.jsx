4c55f34c287c5fd71929c3af6162dc97d4172c2c
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-07-18 00:29:28 +0300

    Update Message.jsx

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -3,7 +3,11 @@ import style from './Message.module.css'
 
 const Message = (props) => {
     return (
-        <div className={style.message}>{props.message}</div>
+        <div className={style.message__wrapper}>
+            <span className={style.message}>
+                {props.message}
+            </span>
+        </div>
     )
 }
 
