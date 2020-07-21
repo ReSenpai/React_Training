8ea5d7ec0ee8f600ec6b90f380179f9c8564779d
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-07-21 17:53:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
+import * as serviceWorker from './serviceWorker';
+import state, { subscribe } from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import state from './redux/state'
+import { addPost, updateNewPostText, updateNewMessageText, sendMessage } from './redux/state';
 
 
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App 
+        state={ state } 
+        addPost={ addPost }
+        sendMessage={ sendMessage }
+        updateNewPostText={ updateNewPostText }
+        updateNewMessageText={ updateNewMessageText } />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App state={ state } />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
