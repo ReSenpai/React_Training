d2fcaf8459a5e4478b33e56f1055348ce401ab6c
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-07-21 17:53:21 +0300

    Update MyPosts.jsx

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,26 +3,49 @@ import style from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
+    
+    let postsElement = props.posts.map((post, i) => <Post
+        key={ i }
+        nickname={ post.nickname }
+        text={ post.text }
+        src={ post.avatar }
+        like={ post.like } />
+    );
 
-    let postsElement = props.posts.map( (post, i) => <Post
-    key={i}
-    nickname={post.nickname}
-    text={post.text}
-    src={post.avatar}
-    like={post.like} /> )
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        props.addPost(); 
+    }
+
+    let onPostChange = () => {
+        let text = newPostElement.current.value;
+        props.updateNewPostText(text);
+    }
+
+    let pressEnter = (event) => {
+        if (event.key === 'Enter') {
+            addPost();
+        }
+    }
 
     return (
         <div>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea className={style.textarea}></textarea>
+                    <textarea
+                        className={ style.textarea }
+                        ref={ newPostElement }
+                        value={ props.newPostText }
+                        onChange={ onPostChange }
+                        onKeyPress={ pressEnter } />
                 </div>
                 <div>
-                    <button>Add Post</button>
+                    <button onClick={ addPost }>Add Post</button>
                 </div>
             </div>
-            <div className={style.posts}>
+            <div className={ style.posts }>
                 { postsElement }
             </div>
         </div>
