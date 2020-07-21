4d071d8f6a642d97089c0ca32280a602441faca5
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-07-21 17:52:56 +0300

    Update Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,33 +2,40 @@ import React from 'react';
 import style from './Dialogs.module.css';
 import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
+import InputMessage from './inputMessage/InputMessage';
 
 const Dialogs = (props) => {
 
     let dialogElements = props.state.dialogs
-    .map( (dialog, i) => <DialogItem 
-        key={ i } 
-        name={ dialog.name } 
-        id={ dialog.id } 
-        avatar={ dialog.avatar } 
-    />)
+        .map((dialog, i) => <DialogItem
+            key={ i }
+            name={ dialog.name }
+            id={ dialog.id }
+            avatar={ dialog.avatar } />
+        );
 
     let messagesElement = props.state.messages
-    .map( (message, i) => <Message 
-        key={ i } 
-        message={ message.message } 
-    />)
+        .map((message, i) => <Message
+            key={ i }
+            message={ message.message } />
+        );
 
     return (
-        <div className={style.dialogs}>
-            <div className={style.dialogs__item}>
+        <div className={ style.dialogs }>
+            <div className={ style.dialogs__item }>
                 { dialogElements }
             </div>
-            <div className={style.messages}>
-                { messagesElement }
+            <div>
+                <div className={ style.messages }>
+                    { messagesElement }
+                </div>
+                <InputMessage
+                    sendMessage={ props.sendMessage }
+                    updateNewMessageText={ props.updateNewMessageText }
+                    newMessageText= { props.newMessageText } />
             </div>
         </div>
-    )
+    );
 }
 
 export default Dialogs;
