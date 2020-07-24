6f5c063739cff9577a847ecd7a5b208143e49577
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-07-24 21:24:35 +0300

    Update MyPosts.jsx

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import style from './MyPosts.module.css';
 import Post from './Post/Post';
+import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/state';
+
 
 const MyPosts = (props) => {
     
@@ -15,12 +17,12 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef();
 
     let addPost = () => {
-        props.addPost(); 
+        props.dispatch(addPostActionCreator()); 
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        props.updateNewPostText(text);
+        props.dispatch(updateNewPostTextActionCreator(text));
     }
 
     let pressEnter = (event) => {
