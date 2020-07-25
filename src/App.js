10f592b79eba70c2ff543bfa45723b0210d53c86
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-07-25 19:11:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Setting from './components/Setting/Setting';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
 
 const App = (props) => {
   return (
@@ -18,12 +18,10 @@ const App = (props) => {
         <div className='app-wrapper-content'>
           <Route path='/profile'
             render={ () => <Profile
-              state={ props.state.profilePage }
-              dispatch={ props.dispatch } />} />
+              store={ props.store } />} />
           <Route path='/messages'
-            render={ () => <Dialogs
-              state={ props.state.dialogsPage }
-              dispatch={ props.dispatch } />} />
+            render={ () => <DialogsContainer
+              store={ props.store } />} />
           <Route path='/news'
             render={ () => <News />} />
           <Route path='/music'
