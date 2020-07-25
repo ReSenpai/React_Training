7319c691b4346909316e7caf5145e6120c288b0b
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-07-25 19:11:33 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import style from './Profile.module.css';
-import MyPosts from './MyPosts/MyPosts';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
+import MyPostsContainer from './MyPosts/MyPostsContainer';
 
 const Profile = (props) => {
     return (
         <div className={ style.wrapper }>
             <ProfileInfo />
-            <MyPosts 
-                posts={ props.state.posts }
-                newPostText={ props.state.newPostText }
-                dispatch={ props.dispatch } />
+            <MyPostsContainer 
+                store={ props.store } />
         </div>
     );
 }
