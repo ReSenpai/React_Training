138487648c08e98397a7c0c3da0108450e59c50f
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-07-28 19:37:55 +0300

    add StoreContext

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import store from './redux/redux_store';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from './StoreContext';
 
 
 let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <React.StrictMode>
-      <App 
-        store={ store }
-         />
+      <Provider store={ store }>
+        <App />
+      </Provider>
     </React.StrictMode>,
     document.getElementById('root')
   );
