1aa2f7a28d429dab5d2323d36e290e3c9297398b
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-08-01 13:54:48 +0300

    Create UsersContainer.jsx

---
+++ src/components/Users/UsersContainer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Users from './Users';
+import { connect } from 'react-redux';
+import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersCountAC } from '../../redux/users_reducer';
+
+const mapStateToProps = (state) => {
+    return {
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        follow: (userId) => {
+            dispatch(followAC(userId));
+        },
+        unfollow: (userId) => {
+            dispatch(unfollowAC(userId));
+        },
+        setUsers: (users) => {
+            dispatch(setUsersAC(users));
+        },
+        setCurrentPage: (pageNumber) => {
+            dispatch(setCurrentPageAC(pageNumber));
+        },
+        setTotalUsersCount: (totalCount) => {
+            dispatch(setTotalUsersCountAC(totalCount));
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Users);
