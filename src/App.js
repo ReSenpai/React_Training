360b44c5eac5b448b4392eeb504a40fbb3323453
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-08-02 23:10:59 +0300

    Replace Profile to ProfileContainer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { HashRouter, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Setting from './components/Setting/Setting';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
+import ProfileContainer from './components/Profile/ProfileContainer';
 
 const App = (props) => {
   return (
@@ -18,7 +18,7 @@ const App = (props) => {
         <Navbar />
         <div className='app-wrapper-content'>
           <Route path='/profile'
-            render={ () => <Profile />} />
+            render={ () => <ProfileContainer />} />
 
           <Route path='/messages'
             render={ () => <DialogsContainer />} />
