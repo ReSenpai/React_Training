eb124168ecabcbb3615eb83f70a6289a103741be
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-08-03 18:49:51 +0300

    add withRouter

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -5,11 +5,14 @@ import { connect } from 'react-redux';
 import { 
     setUserProfile
 } from '../../redux/profile_reducer';
+import { withRouter } from 'react-router-dom';
 
 class ProfileContainer extends React.Component {
 
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+        let userId = this.props.match.params.userId;
+        if (!userId) userId = 2;
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
         .then(response => {
             this.props.setUserProfile(response.data);
         });
@@ -26,6 +29,8 @@ const mapStateToProps = (state) => ({
     profile: state.profilePage.profile
 })
 
+const withUrlDataContainerComponent = withRouter(ProfileContainer);
+
 export default connect(mapStateToProps, {
     setUserProfile
-})(ProfileContainer);
+})(withUrlDataContainerComponent);
