a9a589cdbf4d2945b86d65a05c43ef942db636f8
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-08-03 18:50:32 +0300

    add user info

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import style from './ProfileInfo.module.css';
 import Preloader from '../../common/Preloader/Preloader';
+import SocialMedia from '../../common/SocialMedia/SocialMedia';
 
-const ProfileInfo = (prosp) => {
+const ProfileInfo = (props) => {
 
-    if (!prosp.profile) {
+    if (!props.profile) {
         return <Preloader />
     }
     return (
         <div>
-            <div className={style.img__wrapper}>
-                <img className={style.images} src='https://i.pinimg.com/originals/6f/2d/34/6f2d34fe6c8746c56c14fbc55308ef99.jpg' alt='anime background'></img>
+            <div className={ style.img__wrapper} >
+                <img 
+                    className={ style.images } 
+                    src='https://i.pinimg.com/originals/6f/2d/34/6f2d34fe6c8746c56c14fbc55308ef99.jpg' alt='anime background'></img>
             </div>
-            <div>
-                <img src={ prosp.profile.photos.large } alt="User photo"/>
-                avatar + description
-                </div>
+            <div className={ style.user_photo__wrapper }>
+                <img
+                    className={ style.user_photo } 
+                    src={ props.profile.photos.large } 
+                    alt="User photo"/>
+                <div>{ props.profile.fullName }</div>
+                <div>{ props.profile.aboutMe }</div>
+                <div>{ 
+                    props.profile.lookingForAJob ? 
+                    props.profile.lookingForAJobDescription : 
+                    'Не ищу работу' 
+                }</div>
+                <SocialMedia />
+            </div>
         </div>
 
     )    
