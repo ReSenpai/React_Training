e0baa59149d2e7668b8439615d36edc74d402c58
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-08-03 18:48:05 +0300

    add Riute params 'userId'

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = (props) => {
         <Header />
         <Navbar />
         <div className='app-wrapper-content'>
-          <Route path='/profile'
+          <Route path='/profile/:userId?'
             render={ () => <ProfileContainer />} />
 
           <Route path='/messages'
