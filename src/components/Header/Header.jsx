e4560b1f5076d411eb258b23ed91f818d909fff9
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-08-04 16:33:43 +0300

    Add LoginBlock

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import style from './Header.module.css';
+import styles from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
     return (
-        <header className={style.header}>
-            <img src='https://static.rfstat.com/renderforest/images/v2/logo-homepage/flat_3.png' alt='logo'></img>
+        <header className={styles.header}>
+            <img 
+                src='https://static.rfstat.com/renderforest/images/v2/logo-homepage/flat_3.png' 
+                alt='logo'>
+            </img>
+            <div className={ styles.loginBlock }>
+                { 
+                    props.isAuth ? 
+                    props.login :
+                    <NavLink to={'/login'}>Login</NavLink>
+                }
+            </div>
         </header>
     );
 }
