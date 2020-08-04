711910de9548c05c76370f505409aacfb2137027
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-08-04 16:27:32 +0300

    add auth_reducer

--- src/redux/redux_store.js
+++ src/redux/redux_store.js
@@ -2,11 +2,13 @@ import { createStore, combineReducers } from "redux";
 import profileReducer from './profile_reducer';
 import dialogsReducer from './dialogs_reducer';
 import usersReducer from "./users_reducer";
+import authReducer from "./auth_reducer";
 
 let reducersPack = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 });
 
 let store = createStore(reducersPack);
