26cdee6cfaa750e40889a9badf4b2c61c73b6ba9
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-08-16 16:17:59 +0300

    Refactoring

--- src/components/common/FormsControls/FormsControls.jsx
+++ src/components/common/FormsControls/FormsControls.jsx
@@ -1,34 +1,21 @@
 import React from 'react';
 import styles from './FormControls.module.css';
 
-export const Textarea = ({input, meta, render, ...props}) => {
+const Element = Element => ({ input, meta, ...props }) => {
 
     const hasError = meta.touched && meta.error;
-
+    
     return (
-        <div className={ styles.formControl + ' ' + (hasError && styles.error) }>
+        <div className={ styles.formControl + " " + (hasError && styles.error) }>
             <div>
-                <textarea {...input} {...props} />
+                <Element {...input} {...props} />
             </div>
             <div>
-                { hasError && <span>{ meta.error }</span> }
+                { hasError && <span> { meta.error } </span> }
             </div>
         </div>
-    )
-}
+    );
+};
 
-export const Input = ({input, meta, render, ...props}) => {
-
-    const hasError = meta.touched && meta.error;
-
-    return (
-        <div className={ styles.formControl + ' ' + (hasError && styles.error) }>
-            <div>
-                <input {...input} {...props} />
-            </div>
-            <div>
-                { hasError && <span>{ meta.error }</span> }
-            </div>
-        </div>
-    )
-}
+export const Textarea = Element('textarea');
+export const Input = Element('input');
