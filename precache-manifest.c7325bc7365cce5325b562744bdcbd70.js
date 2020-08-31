a60d230fc5872d5c1d98ccffc53d87e9460135bd
ReSenpai <bezbana1@gmail.com>
2020-08-31 20:56:05 +0300

    Updates

---
+++ precache-manifest.c7325bc7365cce5325b562744bdcbd70.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "995ba54369606a8136a460a4bf29e7ba",
+    "url": "/React_Training/index.html"
+  },
+  {
+    "revision": "7e29c68f902bd371f392",
+    "url": "/React_Training/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c06449458fe3840bdca6",
+    "url": "/React_Training/static/css/3.80aa0ea7.chunk.css"
+  },
+  {
+    "revision": "85444e2962b14dd8ff73",
+    "url": "/React_Training/static/css/4.912356bb.chunk.css"
+  },
+  {
+    "revision": "d3e7f6fae342c37d01de",
+    "url": "/React_Training/static/css/5.2b283795.chunk.css"
+  },
+  {
+    "revision": "d38a9437e4ca0a9b4d39",
+    "url": "/React_Training/static/css/6.77ab2826.chunk.css"
+  },
+  {
+    "revision": "226c9cf3d178e1268c65",
+    "url": "/React_Training/static/css/7.fdff491f.chunk.css"
+  },
+  {
+    "revision": "1a9bab2c23eb00a16f54",
+    "url": "/React_Training/static/css/main.26e1f0d4.chunk.css"
+  },
+  {
+    "revision": "7e29c68f902bd371f392",
+    "url": "/React_Training/static/js/2.d4cd4ef1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React_Training/static/js/2.d4cd4ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c06449458fe3840bdca6",
+    "url": "/React_Training/static/js/3.7ecb73cc.chunk.js"
+  },
+  {
+    "revision": "85444e2962b14dd8ff73",
+    "url": "/React_Training/static/js/4.f944411a.chunk.js"
+  },
+  {
+    "revision": "d3e7f6fae342c37d01de",
+    "url": "/React_Training/static/js/5.d22bfd3f.chunk.js"
+  },
+  {
+    "revision": "d38a9437e4ca0a9b4d39",
+    "url": "/React_Training/static/js/6.b04ebe9d.chunk.js"
+  },
+  {
+    "revision": "226c9cf3d178e1268c65",
+    "url": "/React_Training/static/js/7.438693ce.chunk.js"
+  },
+  {
+    "revision": "1a9bab2c23eb00a16f54",
+    "url": "/React_Training/static/js/main.64334dfb.chunk.js"
+  },
+  {
+    "revision": "8ff1f57a10008d801ffb",
+    "url": "/React_Training/static/js/runtime-main.c23c44fe.js"
+  },
+  {
+    "revision": "223507bbeb748f4f370b04926e6c21d8",
+    "url": "/React_Training/static/media/anonimus.223507bb.jpg"
+  },
+  {
+    "revision": "5d668923090c384871d474b47829a91f",
+    "url": "/React_Training/static/media/facebook.5d668923.svg"
+  },
+  {
+    "revision": "30712bf537fb840b9c41f37295182cca",
+    "url": "/React_Training/static/media/messages.30712bf5.svg"
+  },
+  {
+    "revision": "71dbde5651aad2774b46308799762502",
+    "url": "/React_Training/static/media/music.71dbde56.svg"
+  },
+  {
+    "revision": "784e4801054bf6b60c9643b8caa8c9ff",
+    "url": "/React_Training/static/media/news.784e4801.svg"
+  },
+  {
+    "revision": "cd110eaec48853b9117d35de9110544b",
+    "url": "/React_Training/static/media/profile.cd110eae.svg"
+  },
+  {
+    "revision": "83b5c18019dd260c6999c591e3f44f0e",
+    "url": "/React_Training/static/media/self_improvement.83b5c180.svg"
+  },
+  {
+    "revision": "fba6bd5c8345330ff8d601992b58ddfe",
+    "url": "/React_Training/static/media/setting.fba6bd5c.svg"
+  },
+  {
+    "revision": "db381a12c54316a18e048ba7fe92a112",
+    "url": "/React_Training/static/media/users.db381a12.svg"
+  },
+  {
+    "revision": "c511cafb6bedc8f2c30293a778cb2910",
+    "url": "/React_Training/static/media/vk.c511cafb.svg"
+  },
+  {
+    "revision": "e93cb0afb50b5734ade083903d0a7b6c",
+    "url": "/React_Training/static/media/web-site.e93cb0af.svg"
+  }
+]);
