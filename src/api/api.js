0f2bb11816ed1b9f01c3a6a6088dfffa3266f40f
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-09-03 22:46:30 +0300

    Add api method savePhoto and updateProfile

--- src/api/api.js
+++ src/api/api.js
@@ -66,6 +66,18 @@ export const profileAPI = {
         return instance.put('profile/status', {
             status: status
         });
+    },
+    savePhoto(photoFile) {
+        const formData = new FormData();
+        formData.append('image', photoFile);
+        return instance.put(`profile/photo`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }).then(response => response.data);
+    },
+    updateProfile(profile) {
+        return instance.put('profile', profile).then(response => response.data);
     }
 }
 
