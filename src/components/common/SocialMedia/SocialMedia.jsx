c6150385da791de9a8f99fcd1be8f4acc3ef55df
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-09-03 22:47:26 +0300

    Update SocialMedia.jsx
    Refactoring

--- src/components/common/SocialMedia/SocialMedia.jsx
+++ src/components/common/SocialMedia/SocialMedia.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import style from './SocialMedia.module.css';
+import styles from './SocialMedia.module.css';
 import facebook from '../../../assets/icons/facebook.svg';
 import website from '../../../assets/icons/web-site.svg';
 import { ReactComponent as Vkontakte  } from '../../../assets/icons/vk.svg';
 
 
-const SocialMedia = (props) => {
+const SocialMedia = ({contacts}) => {
+
     return (
-        <div>
-            <img src={ facebook } alt="Facebook logo"/>
-            <img src={ website } alt="Website logo"/>
+        <div className={ styles.wrapper }>
+            { Object.keys(contacts).map(key => {
+                return contacts[key] &&  <a key={ key } href={ contacts[key] }>{ key }</a>
+            }) }
         </div>
     )
 }
