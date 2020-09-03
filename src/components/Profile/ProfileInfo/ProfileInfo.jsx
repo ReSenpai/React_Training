db28d90e14be36a12ca751d1cd44dc2fbc56eeac
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-09-03 22:50:08 +0300

    Add profile info

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,52 +1,99 @@
 import React from 'react';
-import style from './ProfileInfo.module.css';
+import styles from './ProfileInfo.module.css';
 import Preloader from '../../common/Preloader/Preloader';
 import SocialMedia from '../../common/SocialMedia/SocialMedia';
 import ProfileStatus from './ProfileStatus/ProfileStatus';
+import defaultAvatar from '../../../assets/images/defaultAvatar.png';
+import { useState } from 'react';
+import ProfileDataForm from './ProfileDataForm/ProfileDataForm';
 
-const ProfileInfo = ({profile, status, updateUserStatus}) => {
+const ProfileInfo = ({profile, status, updateUserStatus, isOwner, savePhoto, updateProfile}) => {
+
+    let [editMode, setEditMode] = useState(false);
+    const activateEditMode = () => setEditMode(true);
+    const deactivateEditMode = () => setEditMode(false);
+
+    const onMainPhotoSelected = (event) => {
+        if (event.target.files.length) {
+            savePhoto(event.target.files[0]);
+        }
+    }
+
+    const onSubmit = (formData) => {
+        updateProfile(formData);
+        setEditMode(false);
+    }
 
     if (!profile) {
         return <Preloader />
     }
+
     return (
         <div>
-            <div className={ style.img__wrapper} >
+            <div className={ styles.img__wrapper} >
                 <img 
-                    className={ style.images } 
+                    className={ styles.images } 
                     src='https://i.pinimg.com/originals/6f/2d/34/6f2d34fe6c8746c56c14fbc55308ef99.jpg' 
                     alt='anime background'>
                 </img>
             </div>
-            <div className={ style.user_info_container }>
+            <div className={ styles.user_info_container }>
                 <div>
-                    <img
-                        className={ style.user_photo } 
-                        src={ 
-                            profile.photos.large
-                                ? profile.photos.large
-                                : 'https://miro.medium.com/max/720/1*W35QUSvGpcLuxPo3SRTH4w.png' 
-                            } 
-                        alt="User photo"/>
+                    <div className={ styles.userPhotoContainer }>
+                        <img
+                            className={ styles.user_photo } 
+                            src={ profile.photos.large || defaultAvatar } 
+                            alt="User photo"/>
+                        { isOwner 
+                        && <input 
+                            className={ styles.button } 
+                            type='file' 
+                            onChange={ onMainPhotoSelected } />}
+                    </div>
                     <ProfileStatus 
                     status={ status }
                     updateUserStatus={ updateUserStatus } />
                 </div>
-                <div>
-                    <h2>{ profile.fullName }</h2>
-                    <div>{ profile.aboutMe }</div>
-                    <div>{ 
-                        profile.lookingForAJob
-                            ? profile.lookingForAJobDescription
-                            : 'Не ищу работу' 
-                        }
-                    </div>
-                    <SocialMedia />
+                <div className={ styles.infoWrapper }>
+                    { editMode 
+                        ? <ProfileDataForm 
+                            profile={ profile }
+                            onSubmit={ onSubmit }
+                            deactivateEditMode={ deactivateEditMode } /> 
+                        : <ProfileData 
+                            profile={ profile } 
+                            isOwner={ isOwner } 
+                            activateEditMode={ activateEditMode } /> 
+                    }
                 </div>
             </div>
         </div>
 
     )    
+}
+
+const ProfileData = ({profile, isOwner, activateEditMode}) => {
+    return (
+        <div>
+            { isOwner && <div><button onClick={ activateEditMode }>edit</button></div> }
+            <h2>{ profile.fullName }</h2>
+            <div>
+                <b>Поиск работы: </b> { profile.lookingForAJob
+                    ? 'Да'
+                    : 'Нет' 
+                }
+            </div>
+            { profile.lookingForAJob &&
+                <div>
+                    <b>Мои скиллы: </b> { profile.lookingForAJobDescription }
+                </div>
+            }
+            <div>
+                <b>Обо мне: </b> { profile.aboutMe }
+            </div>
+            <SocialMedia contacts={ profile.contacts } />
+        </div>
+    )
 }
 
 export default ProfileInfo;
