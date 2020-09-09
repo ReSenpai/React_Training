d8db1e10fe5bcc04193009ad4504798ab1ba2fd4
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-09-09 08:51:07 +0300

    Typed Users

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import styles from './Users.module.css';
 import Pagination from '../common/Pagination/Pagination';
 import User from './User';
+import { UserType } from '../../types/types';
 
-const Users = ({
+type PropsType = {
+    totalItemsCount: number
+    pageSize: number
+    currentPage: number
+    users: Array<UserType>
+    onPageChanged: (page: number) => void
+    unfollow: (userId: number) => void
+    follow: (userId: number) => void
+    followingInProgress: Array<number>
+}
+
+const Users: React.FC<PropsType> = ({
     totalItemsCount,
     pageSize,
     currentPage,
