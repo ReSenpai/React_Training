fc019cd03af023e48174e0c5f0f90b5103c8db3f
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-09-09 08:52:45 +0300

    Refactoring mapStateToProps on selectors

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import { connect } from 'react-redux';
 import { logout } from '../../redux/auth_reducer';
+import { getIsAuth, getLogin } from '../../redux/auth_selectors';
 
 const HeaderContainer = (props) => {
 
@@ -11,8 +12,8 @@ const HeaderContainer = (props) => {
 }
 
 const mapStateToProps = (state) => ({
-    isAuth: state.auth.isAuth,
-    login: state.auth.login
+    isAuth: getIsAuth(state),
+    login: getLogin(state)
 })
 
 export default connect(mapStateToProps, {
