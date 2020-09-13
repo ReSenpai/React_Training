0abe44009a4b38b2b35b9e5cd666f20400d6d5c7
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-09-13 22:59:27 +0300

    Typed ActionTypes and Thunks

--- src/redux/app_reducer.ts
+++ src/redux/app_reducer.ts
@@ -1,4 +1,7 @@
+import { Dispatch } from "redux";
+import { ThunkAction } from "redux-thunk";
 import { getAuthUserData } from "./auth_reducer";
+import { AppStateType } from "./redux_store";
 
 const SET_INITIALIZATION = 'app/SET_INITIALIZATION';
 
@@ -7,7 +10,7 @@ let initialState = {
 }
 export type InitialStateType = typeof initialState;
 
-const appReducer = (state = initialState, action: any): InitialStateType => {
+const appReducer = (state = initialState, action: ActionsTypes): InitialStateType => {
 
     switch (action.type) {
         case SET_INITIALIZATION: {
@@ -21,13 +24,22 @@ const appReducer = (state = initialState, action: any): InitialStateType => {
     }  
 }
 
+type ActionsTypes = InitializationSuccessActionType;
+
 type InitializationSuccessActionType = { type: typeof SET_INITIALIZATION }
 
 export const initializationSuccess = (): InitializationSuccessActionType => ({ 
     type: SET_INITIALIZATION
 });
 
-export const startInitialization = () => (dispatch: any) => {
+
+type GetStateType = () => AppStateType;
+type DispatchType = Dispatch<ActionsTypes>;
+type ThunkType = ThunkAction<Promise<void>, AppStateType, unknown, ActionsTypes>;
+
+
+export const startInitialization = ()
+: ThunkAction<void, AppStateType, unknown, ActionsTypes> => (dispatch) => {
     const authUserData = dispatch(getAuthUserData());
 
     Promise.all([authUserData])
